22462115cb9610b4470aa169723d417ec887216e
Daniel Parvin <danparvin@gmail.com>
2022-06-21 21:02:33 -0500

    Added BookController, CompositionController, and PoemController.

--- src/main/java/com/parvin/learninglists/data/works/musical/MusicalWork.java
+++ src/main/java/com/parvin/learninglists/data/works/musical/MusicalWork.java
@@ -6,6 +6,11 @@ import javax.persistence.MappedSuperclass;
 import com.parvin.learninglists.data.general.Person;
 import com.parvin.learninglists.data.works.Work;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @MappedSuperclass
 public abstract class MusicalWork extends Work {
 	@ManyToOne
